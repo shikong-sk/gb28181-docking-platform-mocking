5fc20e3d73acbe8c44a55f30f8c0c179b47e4c17
shikong <919411476@qq.com>
2023-09-05 01:01:17 +0800

    项目基础结构完善

--- gb28181-mocking-starter/src/main/java/cn/skcks/docking/gb28181/mocking/Gb28181MockingStarter.java
+++ gb28181-mocking-starter/src/main/java/cn/skcks/docking/gb28181/mocking/Gb28181MockingStarter.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients(basePackages = "cn.skcks.docking.gb28181.media")
 @SpringBootApplication
-@ComponentScan(basePackages = "cn.skcks.docking.gb28181")
+@ComponentScan(basePackages = {
+        "cn.skcks.docking.gb28181.annotation",
+        "cn.skcks.docking.gb28181.common",
+        "cn.skcks.docking.gb28181.mocking",
+})
 public class Gb28181MockingStarter {
     public static void main(String[] args) {
         SpringApplication.run(Gb28181MockingStarter.class, args);
