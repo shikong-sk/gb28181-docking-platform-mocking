03b4d17c427ad2322749fe6f5aebfbae25f864dc
shikong <919411476@qq.com>
2023-09-16 16:39:45 +0800

    docker 打包调整

--- package-docker.sh
+++ package-docker.sh
@@ -2,6 +2,8 @@
 # 用于缓存打包过程下载的依赖
 mkdir repository
 curDir=`pwd`
+repository=${curDir}/repository
+
 if ! test -e ${curDir}/ffmpeg/ffmpeg;then
   xz -d ${curDir}/ffmpeg/ffmpeg-git-amd64-static.tar.xz
   tar -xvf ${curDir}/ffmpeg/ffmpeg-git-amd64-static.tar -C ${curDir}/ffmpeg/
@@ -11,7 +13,7 @@ fi
 
 docker run --name maven --rm \
 	-v ${curDir}:/usr/src/mymaven \
-	-v ${curDir}/repository:/root/.m2/repository \
+	-v ${repository}:/root/.m2/repository \
 	-v ${curDir}/settings.xml:/usr/share/maven/ref/settings.xml \
 	-v /etc/docker/daemon.json:/etc/docker/daemon.json -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
 	-w /usr/src/mymaven \
