8634c8ffa4ef71b97ee5d1f179da3f81b30bc661
shikong <919411476@qq.com>
2023-09-27 10:26:20 +0800

    录像回放 单次上报条数改为 500

--- gb28181-mocking-service/src/main/java/cn/skcks/docking/gb28181/mocking/core/sip/message/processor/message/request/recordinfo/RecordInfoRequestProcessor.java
+++ gb28181-mocking-service/src/main/java/cn/skcks/docking/gb28181/mocking/core/sip/message/processor/message/request/recordinfo/RecordInfoRequestProcessor.java
@@ -82,7 +82,7 @@ public class RecordInfoRequestProcessor {
         recordInfoItemDTO.setDeviceId(device.getGbChannelId());
 
         FromHeader fromHeader = request.getFromHeader();
-        ListUtil.partition(recordInfoItemDTOList,2).forEach(recordList->{
+        ListUtil.partition(recordInfoItemDTOList,500).forEach(recordList->{
             RecordInfoResponseDTO recordInfoResponseDTO = new RecordInfoResponseDTO();
             recordInfoResponseDTO.setSn(recordInfoRequestDTO.getSn());
             recordInfoResponseDTO.setDeviceId(device.getGbChannelId());
